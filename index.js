6bc1595f3fdac406a092129ad1d08e74525f2a43
two-jay <djeeee1272@gmail.com>
2020-08-31 11:31:59 +0900

    :sparkles: feature/#12/RouteSetting

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require("express");
+const express = require('express');
+
+const usersRouter = require('./routes/users');
+// 임시로 유저 스프린트 2에서 필요한 users.js 라우트만 설정
+// 이후 진행하면서 추가하기로 함
+
+const logger = require('morgan');
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
+
+app.use(morgan('dev'));
+app.use('/user', usersRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
