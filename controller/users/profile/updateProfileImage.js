1367273f55dcb0d6e156d6e8941d130efe15d5aa
dokdo2005 <dokdo2005@gmail.com>
2020-09-01 14:09:00 +0900

    :sparkles: add profile image update feature

---
+++ controller/users/profile/updateProfileImage.js
@@ -0,0 +1,26 @@
+const { models } = require("../../../models");
+const jwt = require("jsonwebtoken");
+const users = models.User;
+
+module.exports = {
+  patch: (req, res) => {
+    let token = req.cookies.user;
+    jwt.verify(token, JWT_secret, (err, decoded) => {
+      if (err) {
+        res.status(400).send({ message: "image update fail, invalid file" });
+      } else {
+        users
+          .update(
+            { profileURL: req.file.location },
+            {
+              where: { id: decoded.userid },
+            }
+          )
+          .then(() => res.status(200).send({ image_url: req.file.location }))
+          .catch(() =>
+            res.status(500).send({ message: "image update fail, server error" })
+          );
+      }
+    });
+  },
+};
