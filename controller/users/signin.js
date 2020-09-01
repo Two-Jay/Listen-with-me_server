311576d528cf7186a3c75032149303c5e88660a0
two-jay <djeeee1272@gmail.com>
2020-09-01 13:17:41 +0900

    :bug: Fix/replaceImportSyntax

--- controller/users/signin.js
+++ controller/users/signin.js
@@ -1,18 +1,17 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import Token from 'jsonwebtoken';
+const dotenv = require('dotenv');
+const path = require('path');
+const Token = require('jsonwebtoken');
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 let tokenExpireTime = process.env.NODE_ENV === 'production' ? '6h' : '5m';
 // 개발환경에 따른 토큰 expiretime 설정
 
 const jwt = require('jsonwebtoken');
-// const { user } = require("../../models");
-// model definition 이전에 작업하여 해당 라인을 주석처리 함
+const { models } = require('../models');
+const users = models.User;
 
 module.exports = {
   post: (req, res) => {
     const { email, password } = req.body;
-    let sess = req.session;
 
     users
       .findOne({
