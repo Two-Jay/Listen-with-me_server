b7a14b6d83881b20db9c6171e6b258e44077de27
dokdo2005 <dokdo2005@gmail.com>
2020-09-01 11:32:15 +0900

    :hammer: refactor APIs using JWT

--- controller/users/signout.js
+++ controller/users/signout.js
@@ -1,16 +1,14 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = {
   get: (req, res) => {
-    let sess = req.session;
-    if (sess.userid) {
-      sess.destroy((err) => {
-        if (err) {
-          res.status(500).send({ message: "signout fail, server error" });
-        } else {
-          res.status(204).send({ message: "signout success" });
-        }
-      });
-    } else {
-      res.status(500).send({ message: "signout fail, server error" });
-    }
+    let token = req.cookies.user;
+    jwt.verify(token, JWT_secret, (err) => {
+      if (err) {
+        res.status(500).send({ message: "signout fail, server error" });
+      } else {
+        res.status(204).clearCookie(user).send({ message: "signout success" });
+      }
+    });
   },
 };
--- controller/users/signup.js
+++ controller/users/signup.js
@@ -1,4 +1,4 @@
-const { models } = require('../models');
+const { models } = require("../../../models");
 const users = models.User;
 
 module.exports = {
