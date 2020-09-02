5844b998a8ac27e0f654a8bf320a950040e1b16c
dokdo2005 <dokdo2005@gmail.com>
2020-09-02 14:52:10 +0900

    :sparkles: add editPlaylistTitle API

---
+++ controller/lists/editPlaylistTitle.js
@@ -0,0 +1,23 @@
+const playlist = require("../../../models").PlayList;
+
+module.exports = {
+  post: (req, res) => {
+    let token = req.cookies.user;
+    jwt.verify(token, JWT_secret, (err) => {
+      if (err) {
+        res
+          .status(401)
+          .send({ message: "editPlaylistTitle fail, need authentication" });
+      } else {
+        playlist
+          .update({ title: req.body.title }, { where: { id: req.query.id } })
+          .then(() => res.status(200).send({ title: req.body.title }))
+          .catch(() =>
+            res
+              .status(500)
+              .send({ message: "editPlaylistTitle fail, server error" })
+          );
+      }
+    });
+  },
+};
