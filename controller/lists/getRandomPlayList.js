a44d7337d3ded64d4e0f8a3d6b1f3f2a6306a263
dokdo2005 <dokdo2005@gmail.com>
2020-09-03 11:41:29 +0900

    :sparkles: add getRandomEntry API

---
+++ controller/lists/getRandomPlayList.js
@@ -0,0 +1,38 @@
+const playlist = require("../../../models").PlayList;
+const liked = require("../../../models").likedList;
+const music = require("../../../models").Music;
+const users = require("../../../models").User;
+const jwt = require("jsonwebtoken");
+module.exports = {
+  get: (req, res) => {
+    let token = req.cookies.user;
+    jwt.verify(token, JWT_secret, () => {
+      playlist
+        .findOne()
+        .then((data) => {
+          data.thumbnail = music.findOne({
+            where: { playlist_id: data["id"] },
+          }).thumbnails;
+          data.likeAmount = liked.count({
+            where: { likedList_id: data["id"] },
+          });
+          data.nickname = users.findOne({
+            where: { id: data["owner_id"] },
+          }).nickname;
+        })
+        .then((data) => {
+          let payload = {
+            id: data.id,
+            thumbnails: data.thumbnail,
+            title: data.title,
+            nickname: data.nickname,
+            likeAmount: data.likeAmount,
+          };
+          res.status(200).send(payload);
+        })
+        .catch(() =>
+          res.status(500).send({ message: "random loading fail, server error" })
+        );
+    });
+  },
+};
