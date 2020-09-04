3f1be6bf52581e8868bea4946816ff697fa1e191
dokdo2005 <dokdo2005@gmail.com>
2020-09-04 13:07:13 +0900

    :sparkles: add getRoomStatus API

---
+++ controller/rooms/getRoomStatus.js
@@ -0,0 +1,24 @@
+const rooms = require("../../../models").Room;
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  get: (req, res) => {
+    let token = req.cookies.authorization;
+    jwt.verify(token, process.env.JWT_secret, () => {
+      rooms
+        .findOne({ where: { id: req.query.id } })
+        .then((room) => {
+          if (room) {
+            res.status(200).send(room);
+          } else {
+            res
+              .status(404)
+              .send({ message: "getRoomStatus fail, room not found" });
+          }
+        })
+        .catch(() =>
+          res.status(500).send({ message: "getRoomStatus fail, server error" })
+        );
+    });
+  },
+};
