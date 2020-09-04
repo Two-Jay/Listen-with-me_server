8b92a920fb18d6aaaa07b879965e33b4f96a4c33
two-jay <djeeee1272@gmail.com>
2020-09-04 10:43:04 +0900

    :shirt: change jwtsecret var name

--- controller/users/profile/updateNickname.js
+++ controller/users/profile/updateNickname.js
@@ -1,16 +1,16 @@
-const users = require("../../../models").User;
-const jwt = require("jsonwebtoken");
+const users = require('../../../models').User;
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   patch: (req, res) => {
     let token = req.cookies.authorization;
-    jwt.verify(token, JWT_secret, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_secret, (err, decoded) => {
       users
         .findOne({ where: { nickname: req.body.nickname } })
         .then((data) => {
           if (data) {
             res.status(409).send({
-              message: "unavailable nickname, already exists nickname",
+              message: 'unavailable nickname, already exists nickname',
             });
           } else {
             users
@@ -21,19 +21,19 @@ module.exports = {
                 }
               )
               .then(() =>
-                res.status(200).send({ message: "nickname update success" })
+                res.status(200).send({ message: 'nickname update success' })
               )
               .catch(() =>
                 res
                   .status(500)
-                  .send({ message: "nickname update fail, server error" })
+                  .send({ message: 'nickname update fail, server error' })
               );
           }
         })
         .catch(() =>
           res
             .status(500)
-            .send({ message: "nickname update fail, server error" })
+            .send({ message: 'nickname update fail, server error' })
         );
     });
   },
