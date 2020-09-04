b0588f19cf966b7f9a85692831b2ccfc2914ff27
dokdo2005 <dokdo2005@gmail.com>
2020-09-04 16:50:01 +0900

    :bug: fix JWT token verification code

--- controller/root/ListenAlong.js
+++ controller/root/ListenAlong.js
@@ -1,11 +1,11 @@
-const playlist = require('../../../models').PlayList;
-const users = require('../../../models').User;
-const rooms = require('../../../models').Room;
-const jwt = require('jsonwebtoken');
+const playlist = require("../../../models").PlayList;
+const users = require("../../../models").User;
+const rooms = require("../../../models").Room;
+const jwt = require("jsonwebtoken");
 module.exports = {
   post: (req, res) => {
     let token = req.cookies.authorization;
-    jwt.verify(token, JWT_secret, () => {
+    jwt.verify(token, process.env.JWT_secret, () => {
       users
         .findOne({ where: { nickname: req.body.nickname } })
         .then((user) => playlist.findAll({ where: { owner_id: user.id } }))
@@ -13,7 +13,7 @@ module.exports = {
           if (list) {
             for (let i in list) {
               rooms
-                .findOne({ where: { playlist_id: list[i]['id'] } })
+                .findOne({ where: { playlist_id: list[i]["id"] } })
                 .then((room) => {
                   if (room) {
                     res.status(200).send({ id: room.playlist_id });
@@ -22,13 +22,13 @@ module.exports = {
             }
             res
               .status(202)
-              .send({ message: 'searching success, but room closed' });
+              .send({ message: "searching success, but room closed" });
           } else {
-            res.status(400).send({ message: 'searching fail, bad request' });
+            res.status(400).send({ message: "searching fail, bad request" });
           }
         })
         .catch(() =>
-          res.status(500).send({ message: 'searching fail, server error' })
+          res.status(500).send({ message: "searching fail, server error" })
         );
     });
   },
