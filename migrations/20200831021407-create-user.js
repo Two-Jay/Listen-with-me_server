35dcabe4559850a270104627ea01edf76328ebb2
dokdo2005 <dokdo2005@gmail.com>
2020-09-07 15:38:32 +0900

    :bug: fix sequelize default value problem

--- migrations/20200831021407-create-user.js
+++ migrations/20200831021407-create-user.js
@@ -1,42 +1,45 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       nickname: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       OauthType: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       profileURL: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        defaultValue:
+          "https://lwm-test.s3.ap-northeast-2.amazonaws.com/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%2C+2020-09-07+13-20-34.png",
       },
       profileDescription: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        defaultValue: `안녕하세요. ${data.nickname}입니다.`,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
-  }
+    await queryInterface.dropTable("Users");
+  },
 };
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -17,25 +17,19 @@ module.exports = (sequelize, DataTypes) => {
       nickname: DataTypes.STRING,
       password: DataTypes.STRING,
       OauthType: DataTypes.INTEGER,
-      profileURL: DataTypes.STRING,
-      profileDescription: DataTypes.STRING,
-    },
-    {
-      hooks: {
-        afterCreate: (data, option) => {
-          if (data.profileURL === null) {
-            data.profileURL =
-              'https://lwm-test.s3.ap-northeast-2.amazonaws.com/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%2C+2020-09-07+13-20-34.png';
-          }
-          if (data.profileDescription === null) {
-            data.profileDescription = `안녕하세요. ${data.nickname}입니다.`;
-          }
-        },
+      profileURL: {
+        type: DataTypes.STRING,
+        defaultValue:
+          "https://lwm-test.s3.ap-northeast-2.amazonaws.com/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%2C+2020-09-07+13-20-34.png",
+      },
+      profileDescription: {
+        type: DataTypes.STRING,
+        defaultValue: `안녕하세요. ${data.nickname}입니다.`,
       },
     },
     {
       sequelize,
-      modelName: 'User',
+      modelName: "User",
     }
   );
   return User;
