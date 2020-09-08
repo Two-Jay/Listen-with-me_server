99d051347c88ee476ff9aec7b568cf92a807f8d3
dokdo2005 <dokdo2005@gmail.com>
2020-09-08 20:00:08 +0900

    :bug: fix bugs on createPlaylist API

--- controller/lists/createPlaylist.js
+++ controller/lists/createPlaylist.js
@@ -1,29 +1,31 @@
-const playlist = require('../../models').PlayList;
-const music = require('../../models').Music;
-const jwt = require('jsonwebtoken');
+const playlist = require("../../models").PlayList;
+const music = require("../../models").Music;
+const jwt = require("jsonwebtoken");
 module.exports = {
   post: (req, res) => {
     let token = req.cookies.authorization;
+    let listId;
     jwt.verify(token, process.env.JWT_secret, (err, decoded) => {
       if (err) {
-        res.status(401).send({ message: 'createRoom fail, need signin' });
+        res.status(401).send({ message: "createRoom fail, need signin" });
       } else {
         playlist
-          .create({ title: req.body.list_title, owner_id: decoded.id })
+          .create({ title: req.body.list_title, owner_id: decoded.userid })
           .then((list) => {
+            listId = list.id;
             for (let i in req.body.entries) {
               music.create({
                 playlist_id: list.id,
-                title: req.body.entries[i]['title'],
-                artist: req.body.entries[i]['artist'],
-                musicURL: req.body.entries[i]['musicURL'],
-                thumbnails: req.body.entries[i]['thumbnail'],
+                title: req.body.entries[i]["title"],
+                artist: req.body.entries[i]["artist"],
+                musicURL: req.body.entries[i]["musicURL"],
+                thumbnails: req.body.entries[i]["thumbnail"],
               });
             }
           })
-          .then((list) => res.status(201).send({ playlist_id: list.id }))
+          .then(() => res.status(201).send({ playlist_id: listId }))
           .catch(() =>
-            res.status(500).send({ message: 'createRoom fail, server error' })
+            res.status(500).send({ message: "createRoom fail, server error" })
           );
       }
     });
