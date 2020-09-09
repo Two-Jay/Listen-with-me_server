b8aa466cadfeb6e20605c2ce4a36a7fc6b1b3eef
dokdo2005 <dokdo2005@gmail.com>
2020-09-09 10:28:34 +0900

    :sparkles: add currentListener Routing

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { roomsController } = require('../controller');
+const { roomsController } = require("../controller");
 
-router.get('', roomsController.getRoomStatus.get);
-router.post('', roomsController.createRoom.post);
-router.delete('', roomsController.destroyRoom.delete);
-router.patch('', roomsController.setCurrentMusic.patch);
+router.get("", roomsController.getRoomStatus.get);
+router.post("", roomsController.createRoom.post);
+router.delete("", roomsController.destroyRoom.delete);
+router.patch("", roomsController.setCurrentMusic.patch);
+
+router.get("/listener", roomsController.currentListener.get);
+router.post("/listener", roomsController.currentListener.post);
+router.delete("/listener", roomsController.currentListener.delete);
 
 module.exports = router;
