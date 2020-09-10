462ab602da1a5f7b277f9fbfbbb30a5822293d04
dokdo2005 <dokdo2005@gmail.com>
2020-09-10 11:34:43 +0900

    :bug: fix getLikedList API

--- controller/lists/getLikedList.js
+++ controller/lists/getLikedList.js
@@ -6,39 +6,50 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   get: (req, res) => {
     let token = req.cookies.authorization;
-    let payload = [];
-    jwt.verify(token, process.env.JWT_secret, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_secret, async (err, decoded) => {
+      let payload = [];
+
       if (err) {
         res
           .status(404)
           .send({ message: "likedList Loading fail, invalid userid" });
       } else {
-        liked.findAll({ where: { user_id: decoded.userid } }).then((likes) => {
+        try {
+          let likes = await liked.findAll({
+            where: { user_id: decoded.userid },
+          });
+
           for (let i in likes) {
-            let tempObj = playlist.findOne({
+            let list = await playlist.findOne({
               where: { id: likes[i].likedList_id },
             });
-            musicData = music.findOne({
-              where: { playlist_id: likes[i]["id"] },
+            let musicData = await music.findOne({
+              where: { playlist_id: list.id },
             });
-            tempObj.thumbnail = musicData.thumbnails;
-            tempObj.likeAmount = liked.count({
-              where: { likedList_id: likes[i]["id"] },
+            let likeAmount = await liked.count({
+              where: { likedList_id: list.id },
             });
-            tempObj.audienceAmount = acc.count({
-              where: { playlist_id: likes[i]["id"] },
+            let audienceAmount = await acc.count({
+              where: { playlist_id: list.id },
             });
-            payload.push(tempObj);
+            let tmpObj = {
+              id: list.id,
+              title: list.title,
+              thumbnail: musicData === null ? null : musicData.thumbnails,
+              user_id: list.owner_id,
+              likeAmount: likeAmount,
+              audienceAmount: audienceAmount,
+            };
+
+            payload.push(tmpObj);
           }
           res.status(200).send(payload);
-        });
-        /*
-          .catch(() =>
-            res
-              .status(500)
-              .send({ message: "likedList Loading fail, server error" })
-          );
-          */
+        } catch (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send({ message: "likedList Loading fail, server error" });
+        }
       }
     });
   },
