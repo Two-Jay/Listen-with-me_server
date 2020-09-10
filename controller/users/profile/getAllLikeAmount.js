7d88d5d3ae73ab92d658ac00c169f69b29f10994
dokdo2005 <dokdo2005@gmail.com>
2020-09-10 13:45:35 +0900

    :bug: fix getAllLikeAmount API

--- controller/users/profile/getAllLikeAmount.js
+++ controller/users/profile/getAllLikeAmount.js
@@ -1,32 +1,33 @@
-const liked = require('../../../models').likedList;
-const playlist = require('../../../models').PlayList;
-const jwt = require('jsonwebtoken');
+const liked = require("../../../models").likedList;
+const playlist = require("../../../models").PlayList;
+const jwt = require("jsonwebtoken");
 module.exports = {
   get: (req, res) => {
     let token = req.cookies.authorization;
-    let count = 0;
-    jwt.verify(token, process.env.JWT_secret, (err, decoded) => {
-      playlist.findAll({ where: { owner_id: decoded.userid } }).then((list) => {
+    jwt.verify(token, process.env.JWT_secret, async (err, decoded) => {
+      let count = 0;
+      try {
+        let list = await playlist.findAll({
+          where: { owner_id: decoded.userid },
+        });
         if (list) {
           for (let i in list) {
-            liked
-              .count({ where: { likedList_id: list[i]['id'] } })
-              .then((likes) => {
-                count = count + likes;
-              })
-              .catch(() =>
-                res
-                  .status(500)
-                  .send({ message: 'getAllLikeAmount fail, server error' })
-              );
+            try {
+              let likes = await liked.count({
+                where: { likedList_id: list[i]["id"] },
+              });
+              count = count + likes;
+            } catch (err) {
+              res
+                .status(500)
+                .send({ message: "getAllLikeAmount fail, server error" });
+            }
           }
           res.status(200).send({ likeAmount: count });
-        } else {
-          res
-            .status(400)
-            .send({ message: 'getAllLikeAmount fail, bad request' });
         }
-      });
+      } catch (err) {
+        res.status(400).send({ message: "getAllLikeAmount fail, bad request" });
+      }
     });
   },
 };
