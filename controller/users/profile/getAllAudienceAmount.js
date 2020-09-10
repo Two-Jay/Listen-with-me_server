0f270eb5813aa98bf09d8890e6ab0f888f60b9ba
dokdo2005 <dokdo2005@gmail.com>
2020-09-10 13:40:16 +0900

    :bug: fix getAllAudienceAmount API

--- controller/users/profile/getAllAudienceAmount.js
+++ controller/users/profile/getAllAudienceAmount.js
@@ -1,33 +1,36 @@
-const acc = require('../../../models').AccumulateAudience;
-const playlist = require('../../../models').PlayList;
-const jwt = require('jsonwebtoken');
+const acc = require("../../../models").AccumulateAudience;
+const playlist = require("../../../models").PlayList;
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   get: (req, res) => {
     let token = req.cookies.authorization;
-    let count = 0;
-    jwt.verify(token, process.env.JWT_secret, (err, decoded) => {
-      playlist.findAll({ where: { owner_id: decoded.userid } }).then((list) => {
+    jwt.verify(token, process.env.JWT_secret, async (err, decoded) => {
+      try {
+        let count = 0;
+        let list = await playlist.findAll({
+          where: { owner_id: decoded.userid },
+        });
         if (list) {
           for (let i in list) {
-            acc
-              .count({ where: { playlist_id: list[i]['id'] } })
-              .then((number) => {
-                count = count + number;
-              })
-              .catch(() =>
-                res.status(500).send({
-                  message: 'getAllAudienceAmount fail, server error',
-                })
-              );
+            try {
+              let number = await acc.count({
+                where: { playlist_id: list[i]["id"] },
+              });
+              count = count + number;
+            } catch (err) {
+              res.status(500).send({
+                message: "getAllAudienceAmount fail, server error",
+              });
+            }
           }
           res.status(200).send({ audienceAmount: count });
-        } else {
-          res
-            .status(400)
-            .send({ message: 'getAllAudienceAmount fail, bad request' });
         }
-      });
+      } catch (err) {
+        res
+          .status(400)
+          .send({ message: "getAllAudienceAmount fail, bad request" });
+      }
     });
   },
 };
