b03f59bf4b66b67c676cc631b4fc3fad0b3960d2
dokdo2005 <dokdo2005@gmail.com>
2020-09-11 10:37:04 +0900

    :sparkles: add password encryption

--- controller/users/signin.js
+++ controller/users/signin.js
@@ -1,18 +1,24 @@
-let tokenExpireTime = process.env.NODE_ENV === 'production' ? '1h' : '5m';
+let tokenExpireTime = process.env.NODE_ENV === "production" ? "1h" : "5m";
 // 개발환경에 따른 토큰 expiretime 설정
 
-const jwt = require('jsonwebtoken');
-const users = require('../../models').User;
+const jwt = require("jsonwebtoken");
+const users = require("../../models").User;
+const crypto = require("crypto");
 
 module.exports = {
   post: (req, res) => {
     const { email, password } = req.body;
 
+    let encryptedPassword = crypto
+      .createHash("sha256")
+      .update(password + "quartette")
+      .digest("hex");
+
     users
       .findOne({
         where: {
           email: email,
-          password: password,
+          password: encryptedPassword,
         },
       })
       .then((result) => {
@@ -27,18 +33,18 @@ module.exports = {
             }
           );
 
-          res.status(200).cookie('authorization', token).send({
+          res.status(200).cookie("authorization", token).send({
             email: email,
             nickname: result.nickname,
             profileURL: result.profileURL,
             profileDescription: result.profileDescription,
           });
         } else {
-          res.status(404).send({ message: 'signin fail, invalid user data' });
+          res.status(404).send({ message: "signin fail, invalid user data" });
         }
       })
       .catch((err) => {
-        res.status(500).send({ message: 'signin fail, server error' });
+        res.status(500).send({ message: "signin fail, server error" });
       });
   },
 };
