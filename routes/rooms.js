630cccdf00416d7f7f854ec1e1bfb3bbdde70f35
dokdo2005 <dokdo2005@gmail.com>
2020-09-11 16:26:32 +0900

    :bug: fix routing problem

--- routes/rooms.js
+++ routes/rooms.js
@@ -7,12 +7,12 @@ router.post("", roomsController.createRoom.post);
 router.delete("", roomsController.destroyRoom.delete);
 router.patch("", roomsController.setCurrentMusic.patch);
 
-router.get("/listener", roomsController.currentListener.get);
-router.post("/listener", roomsController.currentListener.post);
-router.delete("/listener", roomsController.currentListener.delete);
+router.get("/listener", roomsController.getCurrentListener.get);
+router.post("/listener", roomsController.addCurrentListener.post);
+router.delete("/listener", roomsController.removeCurrentListener.delete);
 
-router.post('/listener', roomsController.addCurrentListener.post);
-router.remove('/listener', roomsController.removeCurrentListener.delete);
-router.get('/listener', roomsController.getCurrentListener.get);
+router.post("/listener", roomsController.addCurrentListener.post);
+router.delete("/listener", roomsController.removeCurrentListener.delete);
+router.get("/listener", roomsController.getCurrentListener.get);
 
 module.exports = router;
