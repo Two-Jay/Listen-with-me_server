2f50a9e96fbd1045c4ca6d1ecd5d8e63b69bb8a9
two-jay <djeeee1272@gmail.com>
2020-09-11 20:53:25 +0900

    :hammer: #11/chatting function debugging and refactor index.js part for socket server configuration

--- chatsocket.js
+++ chatsocket.js
@@ -1,39 +1,44 @@
+const http = require('http');
+const SocketIO = require('socket.io');
+const app = require('./app.js');
+
+const server = http.createServer(app);
+const io = SocketIO(server);
+
 const moment = require('moment');
 const botname = 'Bot';
 
-module.exports = function (io) {
-  io.on('connetionn', (socket) => {
-    socket.on('joinRoom', ({ playlist_id, user_nickname }) => {
-      socket.join(playlist_id);
-      socket.emit(
+io.on('connetionn', (socket) => {
+  socket.on('joinRoom', ({ playlist_id, user_nickname }) => {
+    socket.join(playlist_id);
+    socket.emit(
+      'message',
+      formatMessage(botname, `어서오세요 ${user_nickname}님! :)`)
+    );
+
+    // 방에 있는 모든 사람들에 대한 대상메세지
+    socket.broadcast
+      .to(playlist_id)
+      .emit(
         'message',
-        formatMessage(botname, `어서오세요 ${user_nickname}님! :)`)
+        formatMessage(user_nickname, `이제 저도 같이 듣고 있어요. :D`)
       );
-
-      // 방에 있는 모든 사람들에 대한 대상메세지
-      socket.broadcast
-        .to(playlist_id)
-        .emit(
-          'message',
-          formatMessage(user_nickname, `이제 저도 같이 듣고 있어요. :D`)
-        );
-    });
-
-    socket.on('chatMessage', ({ playlist_id, user_nickname, message }) => {
-      io.to(playlist_id).emit('message', formatMessage(user_nickname, message));
-    });
-
-    socket.on('disconnect', ({ playlist_id, user_nickname }) => {
-      io.to(playlist_id).emit(
-        'message',
-        formatMessage(
-          user_nickname,
-          '이제 나가볼게요. 다음에 만날 때까지 안녕! :)'
-        )
-      );
-    });
   });
-};
+
+  socket.on('chatMessage', ({ playlist_id, user_nickname, message }) => {
+    io.to(playlist_id).emit('message', formatMessage(user_nickname, message));
+  });
+
+  socket.on('disconnect', ({ playlist_id, user_nickname }) => {
+    io.to(playlist_id).emit(
+      'message',
+      formatMessage(
+        user_nickname,
+        '이제 나가볼게요. 다음에 만날 때까지 안녕! :)'
+      )
+    );
+  });
+});
 
 function formatMessage(user_nickname, message) {
   return {
@@ -42,3 +47,5 @@ function formatMessage(user_nickname, message) {
     time: moment().format('h:mm a'),
   };
 }
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,54 +1,6 @@
-const express = require('express');
-const app = express();
-
+const server = require('./chatsocket.js');
 const port = 4500;
-
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const sessionConfiguration = require('./controller/users/Oauth/session-config');
-const passportConfiguration = require('./controller/users/Oauth/passport-config');
-const dotenv = require('dotenv');
-
-dotenv.config();
-sessionConfiguration(app);
-passportConfiguration(app);
-
-const socketServer = require('http').createServer(app);
-const io = require('socket.io')(socketServer);
-require('./chatsocket.js')(io);
-
-const usersRouter = require('./routes/users');
-const playlistsRouter = require('./routes/playlists');
-const rootRouter = require('./routes/root');
-const roomsRouter = require('./routes/rooms');
-
-// DB sync check
-const models = require('./models/index.js');
-models.sequelize
-  .sync()
-  .then(() => {
-    console.log('DB connection success');
-  })
-  .catch((err) => {
-    console.log('**DB connection fail**');
-    console.log(err);
-  });
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(morgan('dev'));
-app.use('/', rootRouter);
-app.use('/user', usersRouter);
-app.use('/playlist', playlistsRouter);
-app.use('/room', roomsRouter);
-
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+/* Server Activation */
+server.listen(port, () => {
+  console.log(`server listening on ${port}`);
 });
