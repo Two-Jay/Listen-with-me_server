865e837ccfbe8565df3b77b6737926137d85e490
dokdo2005 <dokdo2005@gmail.com>
2020-09-14 14:16:30 +0900

    :hammer: modify signout API

--- controller/users/signout.js
+++ controller/users/signout.js
@@ -9,7 +9,7 @@ module.exports = {
       } else {
         res
           .status(204)
-          .clearCookie("authorization")
+          .set("authorization", "")
           .send({ message: "signout success" });
       }
     });
