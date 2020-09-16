d97c8067cdfb8f17ef31ee42aa37bba901744728
dokdo2005 <dokdo2005@gmail.com>
2020-09-16 10:42:13 +0900

    :bug: fix getRandomPlayList, ListenAlong API

--- controller/root/ListenAlong.js
+++ controller/root/ListenAlong.js
@@ -7,36 +7,40 @@ module.exports = {
     let tokenString = req.get("authorization");
     if (tokenString && tokenString.length > 7) {
       let token = tokenString.substring(7);
-      jwt.verify(token, process.env.JWT_secret, (err) => {
+      jwt.verify(token, process.env.JWT_secret, async (err) => {
         if (err) {
           res.status(401).send({ message: "searching fail, need signin" });
         } else {
-          users
-            .findOne({ where: { nickname: req.body.nickname } })
-            .then((user) => playlist.findAll({ where: { owner_id: user.id } }))
-            .then((list) => {
-              if (list) {
+          let user = await users.findOne({
+            where: { nickname: req.body.nickname },
+          });
+          if (user) {
+            let list = await playlist.findAll({ where: { owner_id: user.id } });
+            if (list) {
+              try {
                 for (let i in list) {
-                  rooms
-                    .findOne({ where: { playlist_id: list[i]["id"] } })
-                    .then((room) => {
-                      if (room) {
-                        res.status(200).send({ id: room.playlist_id });
-                      }
-                    });
+                  let room = await rooms.findOne({
+                    where: { playlist_id: list[i]["id"] },
+                  });
+                  if (room) {
+                    res.status(200).send({ id: room.playlist_id });
+                  }
                 }
                 res
                   .status(202)
                   .send({ message: "searching success, but room closed" });
-              } else {
+              } catch (err) {
+                console.log(err);
                 res
-                  .status(400)
-                  .send({ message: "searching fail, bad request" });
+                  .status(500)
+                  .send({ message: "searching fail, server error" });
               }
-            })
-            .catch(() =>
-              res.status(500).send({ message: "searching fail, server error" })
-            );
+            } else {
+              res.status(400).send({ message: "searching fail, bad request" });
+            }
+          } else {
+            res.status(404).send({ message: "searching fail, user not found" });
+          }
         }
       });
     } else {
