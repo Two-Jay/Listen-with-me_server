9e75700ad70c8bd8518b3568507ed0f4f1897613
dokdo2005 <dokdo2005@gmail.com>
2020-09-16 15:09:17 +0900

    :bug: fix destroyRoom API

--- controller/rooms/destroyRoom.js
+++ controller/rooms/destroyRoom.js
@@ -8,34 +8,33 @@ module.exports = {
     let tokenString = req.get("authorization");
     if (tokenString && tokenString.length > 7) {
       let token = tokenString.substring(7);
-      jwt.verify(token, process.env.JWT_secret, (err) => {
+      jwt.verify(token, process.env.JWT_secret, async (err) => {
         if (err) {
           res.status(401).send({ message: "room destroy fail, need signin" });
         } else {
-          rooms.findOne({ where: { id: req.query.id } }).then((room) => {
-            if (room) {
-              acc
-                .update({ room_id: null }, { where: { room_id: req.query.id } })
-                .then(() => rooms.destroy({ where: { id: req.query.id } }))
-                .then(() =>
-                  audiences.destroy({
-                    where: { playList_id: room.playlist_id },
-                  })
-                )
-                .then(() =>
-                  res.status(204).send({ message: "room destroy success" })
-                )
-                .catch(() =>
-                  res
-                    .status(500)
-                    .send({ message: "room destroy fail, server error" })
-                );
-            } else {
+          let room = await rooms.findOne({ where: { id: req.query.id } });
+          if (room) {
+            try {
+              await acc.update(
+                { room_id: null },
+                { where: { room_id: req.query.id } }
+              );
+              await rooms.destroy({ where: { id: req.query.id } });
+              await audiences.destroy({
+                where: { playList_id: room.playlist_id },
+              });
+              res.status(204).send({ message: "room destroy success" });
+            } catch (err) {
+              console.log(err);
               res
-                .status(404)
-                .send({ message: "room destroy fail, room not found" });
+                .status(500)
+                .send({ message: "room destroy fail, server error" });
             }
-          });
+          } else {
+            res
+              .status(404)
+              .send({ message: "room destroy fail, room not found" });
+          }
         }
       });
     } else {
